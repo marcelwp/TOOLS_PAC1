a921f53374409e789c1fad681e1397f9b0561764
Marcel <marcelwp@uoc.edu>
2022-11-19 19:34:16 +0100

    Built with npm

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 const moment = require('moment');
 const lunersee = moment({year: 2022, month: 10, day:1});
-if(document.getElementById('lunerseeCaption') != null)
-{
-    document.getElementById('lunerseeCaption').innerText = "Taken from Marcel's phone " + lunersee.fromNow();
-}
+document.getElementById('lunerseeCaption').innerText = "Taken from Marcel's phone " + lunersee.fromNow();
+
