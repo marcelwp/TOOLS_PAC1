a7667c691fe3b1027d1485b3c66929d686a4d4c0
Marcel <marcelwp@uoc.edu>
2022-11-25 15:51:00 +0100

    Removing files

--- src/app.js
+++
@@ -1,4 +0,0 @@
-const moment = require('moment');
-const lunersee = moment({year: 2022, month: 10, day:1});
-document.getElementById('lunerseeCaption').innerText = "Taken from Marcel's phone " + lunersee.fromNow();
-
